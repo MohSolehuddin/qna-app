cb208aba5d880f0bd84dbd100571a1d22fef5bd1
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-02-19 18:32:24 +0700

    FEAT: Login with credential

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -6,38 +6,25 @@ import GoogleProvider from "next-auth/providers/google";
 
 import { db } from "~/server/db";
 
-/**
- * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
- * object and keep type safety.
- *
- * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
- */
 declare module "next-auth" {
   interface Session extends DefaultSession {
     user: {
       id: string;
-      // ...other properties
-      // role: UserRole;
     } & DefaultSession["user"];
   }
-
-  // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
-  // }
 }
 
-/**
- * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
- *
- * @see https://next-auth.js.org/configuration/options
- */
-export const authConfig = {
+export const authConfig: NextAuthConfig = {
   providers: [
-    DiscordProvider,
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID ?? "",
+      clientSecret: process.env.DISCORD_CLIENT_SECRET ?? "",
+      allowDangerousEmailAccountLinking: true,
+    }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+      allowDangerousEmailAccountLinking: true,
     }),
     CredentialsProvider({
       name: "Credentials",
@@ -46,32 +33,22 @@ export const authConfig = {
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials) {
+        if (!credentials?.username || !credentials?.password) return null;
+
         const user = await db.user.findUnique({
-          where: {
-            username: credentials?.username,
-          },
+          where: { username: credentials.username },
         });
 
-        // Validasi password (gunakan bcrypt atau hashing di sini)
-        if (user && user.password === credentials?.password) {
+        if (user && user.password === credentials.password) {
           return user;
         }
 
-        return null; // Login gagal jika user tidak ditemukan atau password salah
+        return null;
       },
     }),
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
   ],
   pages: {
-    signIn: "/auth/login",
+    signIn: "/auth/signin",
   },
   adapter: PrismaAdapter(db),
   callbacks: {
@@ -82,5 +59,11 @@ export const authConfig = {
         id: user.id,
       },
     }),
+    redirect: async ({ url, baseUrl }) => {
+      if (url === baseUrl || url.startsWith(baseUrl)) {
+        return baseUrl;
+      }
+      return url;
+    },
   },
-} satisfies NextAuthConfig;
+};
