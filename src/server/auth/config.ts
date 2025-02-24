e534070011c0bd286d93cbd9348becf82cca25e0
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-02-24 11:40:19 +0700

    FIX: user login with jwt strategy for Google Provider and credential

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,9 +1,8 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import argon2 from "argon2"; // Import argon2
-import { randomUUID } from "crypto";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
+import { verifyPassword } from "~/lib/utils/verifyPassword";
 
 import { db } from "~/server/db";
 
@@ -16,9 +15,9 @@ declare module "next-auth" {
 }
 
 export const authConfig: NextAuthConfig = {
-  // session: {
-  //   strategy: "jwt",
-  // },
+  session: {
+    strategy: "jwt",
+  },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
@@ -43,14 +42,19 @@ export const authConfig: NextAuthConfig = {
         });
 
         if (user && user.password) {
-          const isPasswordValid = await argon2.verify(
+          const isPasswordValid = await verifyPassword(
+            credentials?.password,
             user.password,
-            credentials.password,
           );
 
           if (isPasswordValid) {
-            console.log("User logged in:", user);
-            return user;
+            return {
+              id: user.id,
+              name: user.name,
+              email: user.email,
+              image: user.image,
+              username: user.username,
+            };
           }
         }
 
@@ -61,45 +65,37 @@ export const authConfig: NextAuthConfig = {
   pages: {
     signIn: "/auth/signin",
   },
+  secret: process.env.AUTH_SECRET,
   adapter: PrismaAdapter(db),
   callbacks: {
-    async jwt({ token, user }) {
-      console.log("Jwt this your token and user info", token, user);
-      const sessionInfo = {
-        id: token.sub,
-        sessionToken: randomUUID(),
-        userId: user.id,
-        expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30),
-        user: {
-          ...user,
-        },
-      };
-      console.log("This is jwt token", sessionInfo);
-      return sessionInfo;
+    async jwt({ token, user, trigger }) {
+      if (user) {
+        token.id = user.id;
+        token.username = user.username;
+      }
+
+      if (trigger === "update") {
+        // Contoh: Update username jika diubah
+        const updatedUser = await db.user.findUnique({
+          where: { id: token.id },
+        });
+        if (updatedUser) {
+          token.username = updatedUser.username;
+        }
+      }
+
+      return token;
     },
-    session: ({ session, user }) => {
-      console.log("Login success this your session", session, user);
-      const token = {
+
+    async session({ session, token }) {
+      return {
         ...session,
         user: {
           ...session.user,
-          id: user.id,
+          id: token.id,
+          username: token.username,
         },
       };
-      console.log("This is Oauth token", token);
-      return token;
-    },
-    /**
-     * Redirects to the baseUrl if the url is relative or if it starts with the baseUrl.
-     * Otherwise, it returns the original url.
-     * @param {{ url: string, baseUrl: string }} param0
-     * @returns {Promise<string>}
-     */
-    redirect: async ({ url, baseUrl }) => {
-      if (url === baseUrl || url.startsWith(baseUrl)) {
-        return baseUrl;
-      }
-      return url;
     },
   },
 };
